1d9bf1a5b9e634609c5e1814fd5624311f8f872a
Futureblur <itsfutureblur@gmail.com>
2021-10-17 14:08:04 +0200

    Reformat code

--- Engine/Source/Runtime/Event/EventSystem.cpp
+++ Engine/Source/Runtime/Event/EventSystem.cpp
@@ -5,43 +5,43 @@
 
 namespace Iceblur
 {
-    void EventSystem::Initialize()
-    {
-        if (m_Instance)
-        {
-            ICE_FATAL(Error::ETypes::A_SINGLETON, { "EventSystem" });
-        }
+	void EventSystem::Initialize()
+	{
+		if (m_Instance)
+		{
+			ICE_FATAL(Error::ETypes::A_SINGLETON, { "EventSystem" });
+		}
 
-        m_Instance = new EventSystem();
-    }
+		m_Instance = new EventSystem();
+	}
 
-    void EventSystem::Subscribe(IEvent::EventName type, const EventSystem::Callback& trigger)
-    {
-        auto& list = GetInstance()->m_EventList;
+	void EventSystem::Subscribe(IEvent::EventName type, const EventSystem::Callback& trigger)
+	{
+		auto& list = GetInstance()->m_EventList;
 
-        //If the event type already exists, insert the callback there
-        if (list.find(type) != list.end())
-        {
-            list[type].push_back(trigger);
-            return;
-        }
+		//If the event type already exists, insert the callback there
+		if (list.find(type) != list.end())
+		{
+			list[type].push_back(trigger);
+			return;
+		}
 
-        //Event type was not found, create new entry
-        std::vector<Callback> callbackList = { trigger };
-        list.insert(std::pair<IEvent::EventName, std::vector<EventSystem::Callback>>(type, callbackList));
-    }
+		//Event type was not found, create new entry
+		std::vector<Callback> callbackList = { trigger };
+		list.insert(std::pair<IEvent::EventName, std::vector<EventSystem::Callback>>(type, callbackList));
+	}
 
-    void EventSystem::Dispatch(const IEvent& event)
-    {
-        for (const auto& e : GetInstance()->m_EventList)
-        {
-            if (strcmp(e.first, event.GetName()) == 0)
-            {
-                for (const auto& callback : e.second)
-                {
-                    callback(event);
-                }
-            }
-        }
-    }
+	void EventSystem::Dispatch(const IEvent& event)
+	{
+		for (const auto& e : GetInstance()->m_EventList)
+		{
+			if (strcmp(e.first, event.GetName()) == 0)
+			{
+				for (const auto& callback : e.second)
+				{
+					callback(event);
+				}
+			}
+		}
+	}
 }
