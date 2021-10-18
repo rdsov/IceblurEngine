6fc8f1b1aa0c9a6652f3ad3f0fc8bc771a039636
Futureblur <itsfutureblur@gmail.com>
2021-10-18 17:40:46 +0200

    Add example code for event system in EditorApp

--- Engine/Source/Editor/EditorApp.cpp
+++ Engine/Source/Editor/EditorApp.cpp
@@ -7,36 +7,40 @@ using namespace Iceblur;
 class EditorApp : public Application
 {
 public:
-    EditorApp() = default;
+	EditorApp() = default;
 
-    ~EditorApp() override = default;
+	~EditorApp() override = default;
 
-    static void OnMouseMoved(IceEventFn e)
-    {
-        ICE_EVENT_CAST(event, e, MouseMoveEvent);
-        ICE_PRINT(event);
-    }
+	static void OnMouseMoved(IceEventFn e)
+	{
+		ICE_EVENT_CAST(event, e, MouseMoveEvent);
+		ICE_PRINT(event);
+	}
 
-    static void OnWindowResize(IceEventFn e)
-    {
-        ICE_EVENT_CAST(event, e, WindowResizeEvent);
-        ICE_PRINT(event);
-    }
+	static void OnWindowResize(IceEventFn e)
+	{
+		ICE_EVENT_CAST(event, e, WindowResizeEvent);
+		ICE_PRINT(event);
+	}
 
-    static void OnWindowMaximize(IceEventFn e)
-    {
-        ICE_EVENT_CAST(event, e, WindowMaximizeEvent);
-        ICE_PRINT(event);
-    }
+	static void OnWindowMaximize(IceEventFn e)
+	{
+		ICE_EVENT_CAST(event, e, WindowMaximizeEvent);
+		ICE_PRINT(event);
+	}
 
-    void OnPostInit() override
-    {
-        WindowManager::CreateWindow(EWindowType::EditorPopup, { }, true);
-        //EventSystem::Subscribe(MouseMoveEvent::type, &OnMouseMoved);
-        //EventSystem::Subscribe(WindowResizeEvent::type, &OnWindowResize);
-        EventSystem::Subscribe(WindowMaximizeEvent::type, &OnWindowMaximize);
-        Application::OnPostInit();
-    }
+	void OnPostInit() override
+	{
+		WindowManager::CreateWindow(EWindowType::EditorPopup, { }, true);
+
+		/*Examples for event system:
+
+		EventSystem::Subscribe(MouseMoveEvent::type, &OnMouseMoved);
+		EventSystem::Subscribe(WindowResizeEvent::type, &OnWindowResize);
+		EventSystem::Subscribe(WindowMaximizeEvent::type, &OnWindowMaximize);*/
+
+		Application::OnPostInit();
+	}
 };
 
 ICE_ENTRY_POINT(EditorApp)
