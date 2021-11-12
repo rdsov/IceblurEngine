d8c6cc47956c736e0a718e1ffc4cf160f0e50120
Futureblur <itsfutureblur@gmail.com>
2021-11-12 19:43:20 +0100

    Add SpectatorCameraComponent class which can freely fly around

---
+++ Engine/Source/Runtime/Scene/Components/SpectatorCameraComponent.cpp
@@ -0,0 +1,104 @@
+// Copyright (c) 2021 Futureblur. All rights reserved. Visit https://ice.futureblur.com for more information.
+
+#include "SpectatorCameraComponent.h"
+
+#include "Core/InputManager.h"
+
+#include "Scene/Entity.h"
+#include "TransformComponent.h"
+
+#include <cmath>
+
+namespace Iceblur
+{
+	SpectatorCameraComponent::SpectatorCameraComponent()
+	{
+		m_Transform = new TransformComponent();
+		m_Front = glm::vec3(0.0f, 0.0f, -1.0f);
+	}
+
+	void SpectatorCameraComponent::Init()
+	{
+		InputManager::BindKey(ICE_KEY_W, ICE_KEY_HOLD);
+		InputManager::BindKey(ICE_KEY_S, ICE_KEY_HOLD);
+		InputManager::BindKey(ICE_KEY_A, ICE_KEY_HOLD);
+		InputManager::BindKey(ICE_KEY_D, ICE_KEY_HOLD);
+		InputManager::BindKey(ICE_KEY_Q, ICE_KEY_HOLD);
+		InputManager::BindKey(ICE_KEY_E, ICE_KEY_HOLD);
+	}
+
+	void SpectatorCameraComponent::Update(float deltaTime)
+	{
+		CameraComponent::Update(deltaTime);
+
+		glm::vec3 target = glm::vec3(0.0f);
+
+		if (InputManager::GetKeyAction(ICE_KEY_W, ICE_KEY_HOLD))
+		{
+			target += m_Front;
+		}
+
+		if (InputManager::GetKeyAction(ICE_KEY_S, ICE_KEY_HOLD))
+		{
+			target -= m_Front;
+		}
+
+		if (InputManager::GetKeyAction(ICE_KEY_A, ICE_KEY_HOLD))
+		{
+			target -= glm::cross(m_Front, m_Up);
+		}
+
+		if (InputManager::GetKeyAction(ICE_KEY_D, ICE_KEY_HOLD))
+		{
+			target += glm::cross(m_Front, m_Up);
+		}
+
+		if (InputManager::GetKeyAction(ICE_KEY_Q, ICE_KEY_HOLD))
+		{
+			target -= m_Up;
+		}
+
+		if (InputManager::GetKeyAction(ICE_KEY_E, ICE_KEY_HOLD))
+		{
+			target += m_Up;
+		}
+
+		if (target != glm::vec3(0.0f))
+		{
+			m_TargetPosition += glm::normalize(target) * m_Speed * 0.01f;
+		}
+
+		m_Position = glm::mix(m_Position, m_TargetPosition, deltaTime * 10.0f);
+
+		float x = (InputManager::GetMousePosition().GetX() - m_LastMouse.GetX()) * m_MouseSensitivity * deltaTime;
+		float y = (m_LastMouse.GetY() - InputManager::GetMousePosition().GetY()) * m_MouseSensitivity * deltaTime;
+
+		Rotate(x, y);
+
+		m_View = glm::lookAt(m_Position, m_Position + m_Front, m_Up);
+
+		m_LastMouse.SetX(InputManager::GetMousePosition().GetX());
+		m_LastMouse.SetY(InputManager::GetMousePosition().GetY());
+	}
+
+	void SpectatorCameraComponent::Rotate(float x, float y)
+	{
+		m_Yaw += x;
+		m_Pitch += y;
+
+		if (m_Pitch > 89.0f)
+		{
+			m_Pitch = 89.0f;
+		}
+		if (m_Pitch < -89.0f)
+		{
+			m_Pitch = -89.0f;
+		}
+
+		glm::vec3 front(0.0f);
+		front.x = cos(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
+		front.y = sin(glm::radians(m_Pitch));
+		front.z = sin(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
+		m_Front = glm::normalize(front);
+	}
+}
---
+++ Engine/Source/Runtime/Scene/Components/SpectatorCameraComponent.h
@@ -0,0 +1,60 @@
+// Copyright (c) 2021 Futureblur. All rights reserved. Visit https://ice.futureblur.com for more information.
+
+#pragma once
+
+#include "CameraComponent.h"
+#include "Math/VMath.h"
+
+namespace Iceblur
+{
+	class TransformComponent;
+
+	//Acts like a normal camera but with the ability to move around.
+	//The movement behaviour can be changed by overriding the Update() function.
+	class SpectatorCameraComponent : public CameraComponent
+	{
+	public:
+		ICE_COMPONENT_DECL(SpectatorCameraComponent, false)
+
+		SpectatorCameraComponent();
+
+		TransformComponent* Transform() const
+		{
+			return m_Transform;
+		}
+
+		void Init();
+
+		virtual void Update(float deltaTime) override;
+
+		const float GetMouseSensitivity()
+		{
+			return m_MouseSensitivity;
+		}
+
+		void SetMouseSensitivity(float value)
+		{
+			if (m_MouseSensitivity > 0.0f)
+			{
+				m_MouseSensitivity = value;
+			}
+		}
+
+	protected:
+		void Rotate(float x, float y);
+
+	protected:
+		TransformComponent* m_Transform;
+
+		glm::vec3 m_Position = glm::vec3(0.0f);
+		glm::vec3 m_TargetPosition = glm::vec3(0.0f);
+		glm::vec3 m_Front;
+		glm::vec3 m_Up = glm::vec3(0.0f, 1.0f, 0.0f);
+
+		float m_Yaw = 0.0f, m_Pitch = 0.0f;
+		V2D m_LastMouse;
+
+		float m_MouseSensitivity = 40.0f;
+		float m_Speed = 3.0f;
+	};
+}
