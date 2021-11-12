ee5be8cea50812aef9ec6d0551f3df6026615373
Futureblur <itsfutureblur@gmail.com>
2021-11-12 19:25:09 +0100

    Add delta time and fps counter

--- Engine/Source/Runtime/Core/VTime.cpp
+++ Engine/Source/Runtime/Core/VTime.cpp
@@ -4,20 +4,45 @@
 
 #include "IO/Log.h"
 
+#include <GLFW/glfw3.h>
+
 #define _CRT_SECURE_NO_WARNINGS
 
 namespace Iceblur
 {
-    TimeData VTime::GetTimeNow()
-    {
-        std::time_t time = std::time(nullptr);
-        std::tm* timeNow = std::localtime(&time);
+	float VTime::m_CurrentTick;
+	float VTime::m_LastTick;
+	float VTime::m_Accumulated;
+	float VTime::m_LastFrameTime;
+	float VTime::m_FPS;
+	float VTime::m_DeltaTime;
 
-        TimeData data { };
-        data.Hours = timeNow->tm_hour;
-        data.Minutes = timeNow->tm_min;
-        data.Seconds = timeNow->tm_sec;
+	TimeData VTime::GetTimeNow()
+	{
+		std::time_t time = std::time(nullptr);
+		std::tm* timeNow = std::localtime(&time);
 
-        return data;
-    }
+		TimeData data { };
+		data.Hours = timeNow->tm_hour;
+		data.Minutes = timeNow->tm_min;
+		data.Seconds = timeNow->tm_sec;
+
+		return data;
+	}
+
+	void VTime::Update()
+	{
+		m_CurrentTick = (float) glfwGetTime();
+		m_DeltaTime = m_CurrentTick - m_LastFrameTime;
+		m_LastFrameTime = m_CurrentTick;
+
+		m_Accumulated++;
+		if (m_CurrentTick - m_LastTick >= 1.0f)
+		{
+			m_FPS = m_Accumulated;
+			m_Accumulated = 0.0f;
+			m_LastTick = m_CurrentTick;
+			//m_FrameTime = ImGui::GetIO().DeltaTime * 1000.0f;
+		}
+	}
 }
--- Engine/Source/Runtime/Core/VTime.h
+++ Engine/Source/Runtime/Core/VTime.h
@@ -6,28 +6,48 @@
 
 namespace Iceblur
 {
-    struct TimeData
-    {
-        int Hours;
-        int Minutes;
-        int Seconds;
+	struct TimeData
+	{
+		int Hours;
+		int Minutes;
+		int Seconds;
 
-        NODISCARD static inline std::string ToString(int value)
-        {
-            std::string time = ICE_TOS(value);
+		NODISCARD static inline std::string ToString(int value)
+		{
+			std::string time = ICE_TOS(value);
 
-            if (value <= 9)
-            {
-                time = "0" + time;
-            }
+			if (value <= 9)
+			{
+				time = "0" + time;
+			}
 
-            return time;
-        }
-    };
+			return time;
+		}
+	};
 
-    class VTime
-    {
-    public:
-        static TimeData GetTimeNow();
-    };
+	class VTime
+	{
+	public:
+		static TimeData GetTimeNow();
+
+		static const float GetDeltaTime()
+		{
+			return m_DeltaTime;
+		}
+
+		static const float GetFPS()
+		{
+			return m_FPS;
+		}
+
+		static void Update();
+
+	private:
+		static float m_CurrentTick;
+		static float m_LastTick;
+		static float m_Accumulated;
+		static float m_LastFrameTime;
+		static float m_FPS;
+		static float m_DeltaTime;
+	};
 }
